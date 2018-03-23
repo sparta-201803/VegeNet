cc69639e38805312e7b206e01a44b4cd83a93a9c
shota <wvw.1991@outlook.jp>
2018-03-24 08:38:42 +0900

    add products controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'products/index'
+
+  get 'products/new'
+
+  get 'products/create'
+
+  get 'products/edit'
+
+  get 'products/update'
+
+  get 'products/show'
+
+  get 'products/destroy'
+
   # devise_for :users
 end
