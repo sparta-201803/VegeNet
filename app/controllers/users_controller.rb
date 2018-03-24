6a6ab6a330fef669f7c8e0ee35c1aebf0b841736
noopy0415 <noopy0415@gmail.com>
2018-03-24 15:36:53 +0900

    Add product show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,11 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+  
   def edit
     @user = User.find(params[:id])
   end
+
   def show
     @user = User.find(params[:id])
   end
