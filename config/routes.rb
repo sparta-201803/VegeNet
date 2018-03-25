2590e963423ed716c31b5ef7f461e224e7585b34
kouki <miki.0418.kouki@gmail.com>
2018-03-25 09:18:24 +0900

    add buy

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only:[:index,:show,:edit,:new,]
   resources :products do
-    post 'buy', on: :member
+  get 'buy', on: :member
   end
   root 'products#index'
 
