840f1af152f38ad1166a2fe0a0d00c48b4c2f432
noopy0415 <37007364+noopy0415@users.noreply.github.com>
2018-03-25 15:08:22 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,5 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-
-  def destroy
-    # URLで指定した番号のモデルを削除する
-
-    @user = User.find(params[:id])
-    @user = User.destroy
   end
 end
